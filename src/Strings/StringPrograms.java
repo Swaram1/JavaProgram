f70ff231b989612ec30fde82e7e0d0477a6edb1e
Swathi Ramesh <swathi.r1502@gmail.com>
2023-06-19 21:43:42 +0530

    fibbonacci

--- src/Strings/StringPrograms.java
+++ src/Strings/StringPrograms.java
@@ -3,9 +3,21 @@ package Strings;
 public class StringPrograms {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		  
+			    
+			 int n1=0,n2=1,n3,i,count=10;    
+			 System.out.print(n1+" "+n2);   
+			    
+			 for(i=2;i<count;++i) 
+			 {    
+			  n3=n1+n2;    
+			  System.out.print(" "+n3);    
+			  n1=n2;    
+			  n2=n3;    
+			 }    
+			  
+			}
 
 	}
 
-}
+
