c8f0bcaa70081b1b660454f042012450f4a9ee58
Swathi Ramesh <swathi.r1502@gmail.com>
2023-10-19 20:39:17 +0530

    test

---
+++ src/Arraylist/replace.java
@@ -0,0 +1,22 @@
+package Arraylist;
+
+import java.util.Scanner;
+
+public class replace {
+
+	public static void main(String[] args) {
+		
+			// TODO Auto-generated method stub
+			Scanner sc= new Scanner(System.in);
+			System.out.print("Enter your input string bevarsi- ");  
+			String inputstring=  sc.nextLine();
+			String otputstring=inputstring.replace("%20", " ");
+		    System.out.println(otputstring.replace("//", "/"));
+			
+		}
+
+	
+
+	}
+
+
